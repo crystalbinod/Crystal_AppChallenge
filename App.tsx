02aaaddac3ca1a13c9b470a28bd252481bfbf358
crystalbinod <crystalbinod@gmail.com>
2025-10-10 14:53:14 -0700

    lkjdsf

--- App.tsx
+++ App.tsx
@@ -3,8 +3,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import RootNavigator from './navigation/RootNavigator';
 
+/// The main entry point of your app
 export default function App() {
   return (
+    //Wraps over your root navigator which wrap all the other navigation
     <NavigationContainer>
       <RootNavigator />
     </NavigationContainer>
--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
+    //Boost speed for builds and reloads
   api.cache(true);
+  // Return the Babel configuration object
   return {
+    // Use the Expo preset, which includes default Babel settings for Expo projects
     presets: ['babel-preset-expo'],
+    // Add the Reanimated plugin, required for using react-native-reanimated
     plugins: ['react-native-reanimated/plugin'],
   };
 };
